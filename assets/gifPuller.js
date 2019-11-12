4697b0fcd26c205de22c97ce12eca56c33e14799
Kyle Becker <gnefkow87@gmail.com>
2019-11-11 22:06:29 -0500

    Now able to pull one GIF at a time from Giphy according to button query

--- assets/gifPuller.js
+++ assets/gifPuller.js
@@ -28,10 +28,54 @@ var buttonId;
 // Array of initial options:
 var initialOptions = ["Incredibles", "Toy Story", "Cars", "Coco"]
 
-// Create Buttons for the Array
+
+
+
+
+// API Vars:
+
+
+
+
+
+
+
+
+function getGIFs() {
+
+  var apiKey = "D2pGnNn2qgx3MeX8XOdetCWrKQtU0bD9";
+  var queryURL = `http://api.giphy.com/v1/gifs/search?q=${buttonId}&api_key=${apiKey}&limit=5`
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+  .then(function(response) {
+
+    {console.log(response);
+      console.log(`buttonId is ${buttonId}`);
+      console.log(`queryURL is ${queryURL}`);
+      
+  }
+  placeholderBoxEl.style.display = "none";
+
+var testGIF = response.data[1].images.downsized.url;
+
+  var newImage = document.createElement("img");
+  newImage.setAttribute("src", testGIF);
+  gifSpotEl.appendChild(newImage);
+  });
+};
+
+
+
+
+
 
 // ===== ===== ===== ===== FUNCTIONS ===== ===== ===== ===== //
 
+// Create Buttons for the "initial Options" Array
 function createInitialButtons() {
   for (var i = 0; i < initialOptions.length; i++) {
   var newButton = document.createElement("button");
@@ -43,20 +87,19 @@ function createInitialButtons() {
   console.log(newButton);
   }
     onClick();
-
 };
 
 
-
+// ON CLICK: Sets the buttonId variable
 function onClick() {
   console.log("on click:")
   var clickedButton = document.getElementsByClassName("queryButton");
-  
+  // Pulls the ID:
   for (var i = 0; i < clickedButton.length; i++) {
-    clickedButton[i].addEventListener('click', function(i) {
+      clickedButton[i].addEventListener('click', function(i) {
       buttonId = i.originalTarget.id;
       console.log(`clicked ${buttonId}`);
-      return buttonId;
+      getGIFs();
     });
   } 
 };
